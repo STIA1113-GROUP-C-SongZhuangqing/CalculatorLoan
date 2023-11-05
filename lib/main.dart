1c2476973b1d549ba7f4844e077b32e04f43e0a3
Song Zhuangqing <990909song@gmail.com>
2023-11-05 16:01:13 +0800

    Calculator

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
   
-    Future.delayed(const Duration(seconds: 2), () {
+    Future.delayed(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
         MaterialPageRoute(builder: (context) => const MainCalc()), 
